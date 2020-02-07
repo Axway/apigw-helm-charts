f8000b7c856c1b22c8edcc8a7ba9793f915d1732
Aleksandr Nartovich <anartovich@axway.com>
2020-02-07 14:43:59 -0600

    Additional K8s parameters added to the charts

---
+++ apimgr/build-cluster.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+#Prompt for user credentials
+echo "Enter Docker registry credentials"
+read -p "username: " USER
+read -s -p "Password (replace the special chars @,\,$,%,etc. with  \@,\\\\,\$,\%): " PASS
+echo "***************"
+
+# Run helm install with provided values
+
+helm install --values values.yaml --name=${RUNID} --set namespace=${RUNID},cassandra.host0=$(hostname -i),ingress.host=$(hostname -i) --set imageCredentials.username=$USER --set imageCredentials.password=$PASS .
+
+
